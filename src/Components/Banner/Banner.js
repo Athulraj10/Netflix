0eeee2cff637b3e73c77c8cb216ae9f75255b538
athulraj10 <athulraj165@gmail.com>
2023-08-14 20:19:13 +0530

    index changed

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -8,11 +8,17 @@ import './Banner.css'
 function Banner() {
   const [movie,setMovie]=useState()
   useEffect(()=>{
-    axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
-      
-      setMovie(response.data.results[0])
+    axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`)
+    .then((response)=>{
+      const results=response.data.results;
+      const randomIndex=Math.floor(Math.random()*results.length);
+      setMovie(response.data.results[randomIndex])
     })
   },[])
+
+
+  
+
   return (
     <div style={{backgroundImage:`url(${movie?imageUrl+movie.backdrop_path:""})`}} className='banner'>
       <div className='content'>
